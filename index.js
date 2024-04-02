824886eacdc2283eb1eab9b6bd72f64f19ac6219
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-03 02:02:42 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+//making a function to check if its both true or not
+function checkingboth(val1, val2) { return val1 && val2; }
+;
+console.log(checkingboth(true, false));
---
+++ index.ts
@@ -0,0 +1,6 @@
+//making a function to check if its both true or not
+function checkingboth (val1: boolean , val2:boolean): boolean{ return val1 && val2};
+
+
+ console.log(checkingboth(true,false));
+
